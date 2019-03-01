ea23bc0be8a2f892d065630c66bf4f6c9d2ebbca
Stocky Aicha <aicha.stocky@gfi.fr>
2019-03-01 15:20:54 +0100

    rajout class Annee

---
+++ Annee.java
@@ -0,0 +1,37 @@
+public class Annee{
+
+	private int jour;
+	private int annee;
+
+	public Annee(int jour,int annee){
+
+
+	}
+  public int getJour(){
+
+	}
+
+	public int getAnnee(){
+	}
+
+  public int setJour(){
+    this.jour = jour;
+  }
+
+
+	public int setAnnee(){
+		this.annee = annee;
+	}
+	public int setJour(){
+		this.jour = jour;
+	}
+
+	public int ajoutJour(){
+		this.jour += jour;
+	}
+
+  public int ajoutAnne(){
+    this.annee += annee;
+  }
+
+}
