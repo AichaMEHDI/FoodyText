f6a3339ce95abaa22ed0f61597e3d183b52b9bc7
Stocky Aicha <aicha.stocky@gfi.fr>
2019-03-04 10:24:38 +0100

    rajout de la classe Mois.java

---
+++ Mois.java
@@ -0,0 +1,35 @@
+public class Mois{
+
+	private int mois;
+	private int semaine ;
+
+	public Annee(int mois,int semaine){
+
+
+	}
+  public int getMois(){
+
+	}
+
+	public int getSemaine(){
+	}
+
+  public int setMois(){
+    this.mois = mois;
+  }
+
+
+	public int setSemaine(){
+		this.semaine = semaine;
+	}
+
+
+	public int ajoutMois(){
+		this.mois += mois;
+	}
+
+  public int ajoutSemaine(){
+    this.semaine += semaine;
+  }
+
+}
